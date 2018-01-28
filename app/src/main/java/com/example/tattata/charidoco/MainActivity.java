2e4610b0f77bb98c6ecfc65690333b0a358082d1
tanopanta <rainstorm2525@gmail.com>
2018-01-29 02:59:16 +0900

    保存機能を追加

--- app/src/main/java/com/example/tattata/charidoco/MainActivity.java
+++ app/src/main/java/com/example/tattata/charidoco/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.tattata.charidoco;
 
 import android.app.TimePickerDialog;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +13,8 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener{
 
     EditText editTime;
+    EditText editParkingID;
+    EditText editMemo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,10 +27,32 @@ public class MainActivity extends AppCompatActivity implements TimePickerDialog.
                 timeFragment.show(getSupportFragmentManager(), "timePicker");
             }
         });
+        editParkingID = findViewById(R.id.editParkingID);
+        editMemo = findViewById(R.id.editMemo);
+        loadData();
     }
 
     @Override
     public void onTimeSet(TimePicker timePicker, int hour, int minute) {
         editTime.setText(String.format(Locale.US, "%02d:%02d", hour,minute));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        saveData();
+    }
+    private void saveData() {
+        SharedPreferences.Editor editor = getSharedPreferences("ChariDoco", MODE_PRIVATE).edit();
+        editor.putString("parkingTime", editTime.getText().toString());
+        editor.putString("parkingID", editParkingID.getText().toString());
+        editor.putString("memo", editMemo.getText().toString());
+        editor.apply();
+    }
+    private void loadData() {
+        SharedPreferences pref = getSharedPreferences("ChariDoco", MODE_PRIVATE);
+        editTime.setText(pref.getString("parkingTime", ""));
+        editParkingID.setText(pref.getString("parkingID", ""));
+        editMemo.setText(pref.getString("memo", ""));
     }
 }
